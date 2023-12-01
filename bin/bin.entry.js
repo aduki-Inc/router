b382901aab906d8aae67754190500b278ae7f92c
fescii <isfescii@gmail.com>
2023-12-01 17:41:57 +0300

    Initialization Entry A04

--- bin/bin.entry.js
+++ bin/bin.entry.js
@@ -2,15 +2,6 @@ const path  = require("path");
 const fs = require('fs').promises;
 
 
-// Generate the index.html
-// language=HTML
-// noinspection HtmlUnknownTarget
-const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
       </head>
       <body>
